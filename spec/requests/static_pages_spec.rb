a60fd542651801c86b7ca16193ec087e6e6bbd2d
Dave J. Patel <davejpatel@gmail.com>
2014-09-02 11:11:48 -0700

    Refactor using let to create base_title variable

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,63 +2,61 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+  let(:base_title) { "Blog App" }
 
+  describe "Home page" do
     it "should have the content 'Blog App'" do 
       visit '/static_pages/home'
 
-      expect(page).to have_content('Blog App')
+      expect(page).to have_content("Blog App")
     end
 
     it "should have the title 'Home'" do 
       visit '/static_pages/home'
 
-      expect(page).to have_title('Blog App | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
   describe "Help page" do
-
     it "should have the content 'Help'" do
       visit '/static_pages/help'
 
-      expect(page).to have_content('Help')
+      expect(page).to have_content("Help")
     end
 
     it "should have the title 'Help'" do 
       visit '/static_pages/help'
 
-      expect(page).to have_title('Blog App | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
-  describe "About page" do
-    
+  describe "About page" do    
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
 
-      expect(page).to have_content('About Us')
+      expect(page).to have_content("About Us")
     end
 
     it "should have the title 'About Us'" do 
       visit '/static_pages/about'
 
-      expect(page).to have_title('Blog App | About Us')
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
-  describe "Contact page" do
-    
+  describe "Contact page" do    
     it "should have the content 'Contact Us'" do
       visit '/static_pages/contact'
 
-      expect(page).to have_content('Contact Us')
+      expect(page).to have_content("Contact Us")
     end
 
     it "should have the title 'Contact Us'" do 
       visit '/static_pages/contact'
 
-      expect(page).to have_title('Blog App | Contact Us')
+      expect(page).to have_title("#{base_title} | Contact Us")
     end
   end
 end
